7d956739147baa79be068b1d43f023394b7a7916
Joel Gabriel <joel@introvertedpixel.com>
2020-05-07 15:30:04 +1000

    premake5 tweaks

--- premake5.lua
+++ premake5.lua
@@ -78,7 +78,7 @@ project "Infinity"
 
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 		
 	filter "configurations:Debug"
